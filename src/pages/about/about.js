ade6f5d979480a17ebcb5fe8cafc17f6ed66e6db
nyan <nyan969@ya.ru>
2019-09-01 21:39:08 +0300

    правка ссылок на картинки, сделан импорт стилей для glidejs

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,4 +1,6 @@
 import "../../style.css";
+import "../../../node_modules/@glidejs/glide/dist/css/glide.core.min.css"
+import "../../../node_modules/@glidejs/glide/dist/css/glide.theme.min.css"
 
 import Glide from '@glidejs/glide';
 
