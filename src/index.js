bb07d2e653eab98066e46f30638e3acba85b9f2a
Nyan969 <bellatrisa.lestrange@gmail.com>
2019-09-13 08:58:57 +0300

    нажатие кнопки клавишей Enter

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import "./style.css";
 import Request from './request';
 import NewsCardList from './news-card-list';
 
+document.forms.search.addEventListener('submit', buttonClick);
 
-const searchButton = document.querySelector('.search-section__button');
-searchButton.addEventListener('click', function () {
+function buttonClick(event) {
+    event.preventDefault();
     const Keywords = document.forms.search.elements.Keywords.value;
-    new NewsCardList().create(new Request().news(Keywords));
-});
+    new NewsCardList().create(new Requests().news(Keywords));
+}
