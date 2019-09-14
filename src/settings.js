dfd3bf98a23268e6c8458e6ca3de36660dd72d39
Nyan969 <bellatrisa.lestrange@gmail.com>
2019-09-15 00:02:17 +0300

    добавлен объект со строковыми значениями месяца

--- src/settings.js
+++ src/settings.js
@@ -17,3 +17,18 @@ export const API_SETTINGS = {
     NEW_PARAMS: `from=${yearWeekAgo}-${mountWeekAgo + 1}-${dayWeekAgo}&to=${year}-${mount + 1}-${day}&language=ru&pageSize=100&apiKey=362638abff334f7eb40cab5ccaab36a3`,
     GIT_URL: 'https://api.github.com/repos/Nyan969/NewsAnalyzer/commits',
 };
+
+export const MONTHS = {
+    0: 'января',
+    1: 'февраля',
+    2: 'марта',
+    3: 'апреля',
+    4: 'мая',
+    5: 'июня',
+    6: 'июля',
+    7: 'августа',
+    8: 'сентября',
+    9: 'октября',
+    10: 'ноября',
+    11: 'декабря'
+};
