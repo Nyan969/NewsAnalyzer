64994f763b0d510b30c5a0d49974b5321f57f246
Nyan969 <bellatrisa.lestrange@gmail.com>
2019-09-18 12:14:29 +0300

    удален console.log

--- src/analytics/analytics.js
+++ src/analytics/analytics.js
@@ -2,4 +2,3 @@ import "../style.css";
 import Statistics from '../statistics'
 
 new Statistics(localStorage.getItem('keywords'), JSON.parse(localStorage.getItem('data')));
-console.log(JSON.parse(localStorage.getItem('data')));
