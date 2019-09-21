b36dfff4737fdd845cee275ff1953a97509054af
Nyan969 <bellatrisa.lestrange@gmail.com>
2019-09-21 11:39:33 +0300

    Изменения в аналитике(расчитывается сколько новостей вышло в каждый из последних семи дней)

--- src/statistics.js
+++ src/statistics.js
@@ -1,31 +1,39 @@
 import {WEEKDAYS} from "./settings";
 
 export default class Statistics {
+
     constructor(keywords, data) {
         this.keywords = keywords;
         this.data = data;
         this.countTitleResults = this.countTitleResults.bind(this);
+        this.getArticlesPerDay = this.getArticlesPerDay.bind(this);
         this.displayGeneralStatistics = this.displayGeneralStatistics.bind(this);
         this.plottingDailyStatistics = this.plottingDailyStatistics.bind(this);
         this.countTitleResults();
+        this.getArticlesPerDay();
         this.plottingDailyStatistics();
     }
 
     countTitleResults() {
-        this.articlesPerDay = {};
         this.count = 0;
         this.data.articles.forEach(item => {
             if (item.title.toLowerCase().includes(this.keywords.toLowerCase())) {
                 this.count++;
-                const date = new Date(item.publishedAt.substring(0, 10)).getDate();
-                if (date in this.articlesPerDay) {
-                    this.articlesPerDay[date]++;
-                } else {
-                    this.articlesPerDay[date] = 1;
-                }
             }
         });
+    }
+
+    getArticlesPerDay() {
+        this.articlesPerDay = {};
+        this.data.articles.forEach(item => {
+        const date = new Date(item.publishedAt.substring(0, 10)).getDate();
+        if (date in this.articlesPerDay) {
+            this.articlesPerDay[date]++;
+        } else {
+            this.articlesPerDay[date] = 1;
+        }
         this.displayGeneralStatistics();
+        });
     }
 
     displayGeneralStatistics() {
@@ -38,12 +46,12 @@ export default class Statistics {
         let firstDay = new Date(new Date().getTime() - 6 * 24 * 60 * 60 * 1000);
         for (let i = 0; i <= 6; i++) {
             const dayInMillisecond = i * 24 * 60 * 60 * 1000;
-            let date =  new Date(firstDay.getTime() + dayInMillisecond);
-            let day = date.getDate();
-            let wday = WEEKDAYS[`${date.getDay()}`];
+            const date =  new Date(firstDay.getTime() + dayInMillisecond);
+            const day = date.getDate();
+            const wday = WEEKDAYS[`${date.getDay()}`];
             document.querySelector(`.day${i}`).textContent =`${day}, ${wday}`;
             if (day in this.articlesPerDay) {
-                const percent = this.articlesPerDay[`${day}`] * 100 / this.count;
+                const percent = this.articlesPerDay[`${day}`] * 100 / this.data.totalResults;
                 document.querySelector(`.graph${i}`).setAttribute('width', `${percent}%`);
                 document.querySelector(`.value${i}`).textContent = `${this.articlesPerDay[`${day}`]}`;
             } else {
